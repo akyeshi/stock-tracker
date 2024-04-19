268a1fbb85bdbe984747c2a1cdde4801e490186f
akyeshi <akyeshi@outlook.com>
2024-04-19 16:26:26 -0400

    header.ejs with navbars, footer.ejs

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const methodOverride = require("method-override"); 
+const methodOverride = require("method-override");
 
-require('dotenv').config(); 
-
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const stocksRouter = require("./routes/stocks");
 
 const app = express();
 
@@ -23,11 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(methodOverride("_method")); 
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/stocks", stocksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
