3fab403665998d67ed193e105cf5f7f13714f310
akyeshi <akyeshi@outlook.com>
2024-04-19 17:05:40 -0400

    session middleware added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
 const methodOverride = require("method-override");
 
 require("dotenv").config();
@@ -21,6 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
