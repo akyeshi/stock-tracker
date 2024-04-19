66d38490e781464e10de6667cf4c91d30e9568e1
akyeshi <akyeshi@outlook.com>
2024-04-19 14:46:06 -0400

    method-override middleware setup

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const methodOverride = require("method-override"); 
 
 require('dotenv').config(); 
+
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -21,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method")); 
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
